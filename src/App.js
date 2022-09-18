ed1758f94150a1628aebe0891dd74179b89abf46
podoco <103816228+podoco@users.noreply.github.com>
2022-09-18 22:15:15 +0900

    할 일 체크(삼항연산자)

--- src/App.js
+++ src/App.js
@@ -20,10 +20,11 @@ export default class App extends Component {
     value: ""
   }
 
-  getStyle = () => {
+  listStyle = (completed) => {
     return {
       padding: "10px",
       borderBottom: "1px #ccc dotted",
+      textDecoration: completed ? "line-through" : "none"
     }
   }
 
@@ -35,8 +36,8 @@ export default class App extends Component {
 
   handleChange = (e) => {
     this.setState({ value: e.target.value });
-
   }
+
   handleSubmit = (e) => {
     e.preventDefault();
 
@@ -48,8 +49,18 @@ export default class App extends Component {
     };
     //원래 있던 할 일에 새로운 할 일 더해주기
     this.setState({ todoData: [...this.state.todoData, newTodo] });
+    this.setState({ value: "" });
   }
 
+  handleCompleChange = (id) => {
+    let newTodoData = this.state.todoData.map((data) => {
+      if (data.id === id) {
+        data.completed = !data.completed;
+      }
+      return data;
+    });
+    this.setState({ todoData: newTodoData });
+  }
 
   render() {
     return (
@@ -60,10 +71,15 @@ export default class App extends Component {
           </div>
 
           {this.state.todoData.map((data) => (
-            <div style={this.getStyle()} key={data.id}>
-              <input type="checkbox" defaultChecked={false} />
-              {data.title}
-              <button className='btnStyle' onClick={() => this.handleClick(data.id)}>x</button>
+            <div style={this.listStyle(data.completed)} key={data.id}>
+              <p>
+                <input
+                  onChange={() => this.handleCompleChange(data.id)}
+                  type="checkbox"
+                  defaultChecked={false} />
+                {data.title}
+                <button className='btnStyle' onClick={() => this.handleClick(data.id)}>x</button>
+              </p>
             </div>
           ))}
 
