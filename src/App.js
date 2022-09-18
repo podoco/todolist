0d88a8fc4d76b8eab291918a49f5b154398df13b
podoco <103816228+podoco@users.noreply.github.com>
2022-09-18 20:15:47 +0900

    할 일 목록 ui만들기(jsx,css)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 
-function App() {
-  return (
-    <div>
-    </div>
-  )
-}
+export default class App extends Component {
 
-export default App;
+  getStyle = () => {
+    return {
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration: "none"
+    }
+  }
+
+  render() {
+    return (
+      <div className='container'>
+        <div className='todoBlock'>
+          <div className='title'>
+            <h1>할일목록</h1>
+          </div>
+
+          <div style={this.getStyle()}>
+            <input type="checkbox" defaultChecked={false} />
+            공부하기
+            <button className='btnStyle'>x</button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
