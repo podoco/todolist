e96ccafad315f6936f0f050858021ccccbdd534d
podoco <103816228+podoco@users.noreply.github.com>
2022-09-22 22:47:14 +0900

    수정기능 추가하기

--- src/components/List.js
+++ src/components/List.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const List = React.memo(({
   id, title, completed, todoData, setTodoData, provided, snapshot
 }) => {
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTitle, setEditedTitle] = useState(title);
 
   const handleCompleChange = (id) => {
 
@@ -15,37 +18,83 @@ const List = React.memo(({
     setTodoData(newTodoData);
   }
 
-
   //삭제하기
   const handleClick = (id) => {
     let newTodoData = todoData.filter(data => data.id !== id)
     setTodoData(newTodoData);
   }
-  return (
-    <div
-      key={id}
-      {...provided.draggableProps}
-      ref={provided.innerRef}
-      {...provided.dragHandleProps}
-      className={`${snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
-        } flex items-center justify-between w-full px-4 py-1 my-2text-gray-600 bg-gray-100 border rounded`}
-    >
+  const handleEditChange = (event) => {
+    setEditedTitle(event.target.value);
+  }
 
-      <div className='items-center'>
-        <input
-          onChange={() => handleCompleChange(id)}
-          type="checkbox"
-          defaultChecked={completed}
-        />{" "}
-        <span className={completed ? "line-through" : undefined}>
-          {title}
-        </span>
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    let newTodoData = todoData.map(data => {
+      if (data.id === id) {
+        data.title = editedTitle
+      }
+      return data;
+    })
+
+    setTodoData(newTodoData)
+    setIsEditing(false)
+  }
+
+  //수정시 모드
+  if (isEditing) {
+    return (
+      <div
+
+        className={`flex items-center justify-between w-full px-4 py-1 my-2text-gray-600 bg-gray-100 border rounded`}>
+        <div className='items-center'>
+          <form onSubmit={handleSubmit}>
+            <input
+              value={editedTitle}
+              onChange={handleEditChange}
+              className="w-full px-3 py-2 mr-4 text-gray-500 rounded"
+            />
+          </form>
+        </div>
+        <div className='items-center'>
+          <button className='px-4 py-2 float-right' onClick={() => setIsEditing(false)} >x</button>
+          <button className='px-4 py-2 float-right' type="submit" onClick={handleSubmit}>save</button>
+        </div>
       </div>
-      <div className='items-center'>
-        <button className='px-4 py-2 float-right' onClick={() => handleClick(id)}>x</button>
+    )
+    //기본 모드
+  } else {
+    return (
+      <div
+        key={id}
+        {...provided.draggableProps}
+        ref={provided.innerRef}
+        {...provided.dragHandleProps}
+        className={`${snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
+          } flex items-center justify-between w-full px-4 py-1 my-2text-gray-600 bg-gray-100 border rounded`}
+      >
+
+        <div className='items-center'>
+          <input
+            onChange={() => handleCompleChange(id)}
+            type="checkbox"
+            defaultChecked={completed}
+          />{" "}
+          <span className={completed ? "line-through" : undefined}>
+            {title}
+          </span>
+        </div>
+        <div className='items-center'>
+          <button className='px-4 py-2 float-right' onClick={() => handleClick(id)}>x</button>
+          <button className='px-4 py-2 float-right' onClick={() => setIsEditing(true)}>edit</button>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
+
+
+
+
 }
 );
 export default List;
